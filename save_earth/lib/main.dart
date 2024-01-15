b58cfd1130bac24d0f758ae1c3c21ab9ed33caeb
John4E656F <jm49andres@gmail.com>
2024-01-15 18:47:32 +0100

    Create Folder structure

--- save_earth/lib/main.dart
+++ save_earth/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:save_earth/save_earth.dart';
+import 'package:save_earth/src/save_earth.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
+
   SaveEarth game = SaveEarth();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget(game: kDebugMode ? SaveEarth() : game));
 }
